b30d00352fe21bb5849ff795b388917e03ea8df3
deedee <mymusese@gmail.com>
2018-08-22 19:04:15 +0900

    validate 메시지 내용 수정 및 네이버 체크아웃시 m_redirect_url 필수입력 조건 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -28,14 +28,14 @@ export function validateProps(userCode, data) {
   if (language && pg !== 'paypal') {
     if (EN_AVAILABLE_PG.indexOf(pg) !== -1) { 
       if (LANGUAGE.indexOf(language) !== -1) {
-        return { validate: false, message: '올바르지 않은 언어 설정입니다.\n 선택하신 PG사는 "ko" 또는 "en" 옵션을 지원합니다. ' };    
+        return { validate: false, message: '올바르지 않은 언어 설정입니다.\n 선택하신 PG사는 ko 또는 en 옵션을 지원합니다. ' };    
       } 
     } else if (language !== 'ko') {
-      return { validate: false, message: '올바르지 않은 언어 설정입니다.\n 선택하신 PG사는 "ko" 옵션만 지원합니다.' };    
+      return { validate: false, message: '올바르지 않은 언어 설정입니다.\n 선택하신 PG사는 ko 옵션만 지원합니다.' };    
     }
   }
   if (pay_method === 'phone' && typeof digital === 'undefined') {
-    return { validate: false, message: '휴대폰 소액결제시 "digital"은 필수입력입니다.' };
+    return { validate: false, message: '휴대폰 소액결제시 digital은 필수입력입니다.' };
   }
   if (pg === 'eximbay') {
     return { validate: false, message: '해당 모듈은 현재 엑심베이 지원을 위한 개발을 진행중입니다.' };
@@ -45,25 +45,25 @@ export function validateProps(userCode, data) {
   }
   if (pg === 'paypal') {
     if (popup === true) {
-      return { validate: false, message: '해당 모듈은 페이팔 또는 네이버 결제시\n팝업 방식은 지원하지 않습니다.' };
+      return { validate: false, message: '해당 모듈에서 popup은\n페이팔 결제시 지원하지 않습니다.' };
     }
     if (!m_redirect_url) {
-      return { validate: false, message: '해당 모듈은 페이팔 또는 네이버 결제시\n"m_redirect_url"은 필수입력입니다.' };
+      return { validate: false, message: '해당 모듈에서 m_redirect_url은\n페이팔 결제시 필수입력입니다.' };
     }
   }
-  if (pg === 'naverpay') {
+  if (pg === 'naverpay' || pg === 'naverco') {
     if (naverPopupMode === true) {
-      return { validate: false, message: '해당 모듈은 네이버 결제시 팝업 방식은 지원하지 않습니다.' };
+      return { validate: false, message: '해당 모듈에서 popup은\n네이버 페이 결제시 지원하지 않습니다.' };
     }
     if (!m_redirect_url) {
-      return { validate: false, message: '해당 모듈은 네이버 결제시 "m_redirect_url"은 필수입력입니다.' };
+      return { validate: false, message: '해당 모듈에서 m_redirect_url은\n네이버 페이 결제시 필수입력입니다.' };
     }
   }
   if (pg === 'danal_tpay' && pay_method === 'vbank' && !biz_num) {
-    return { validate: false, message: '다날-가상계좌시 "biz_num"은 필수입력입니다.' };
+    return { validate: false, message: '다날-가상계좌시 biz_num은 필수입력입니다.' };
   }
   if ((pg === 'kcp_billing' || pg === 'syrup' ) && !customer_uid) {
-    return { validate: false, message: '정기결제시 "customer_uid"는 필수입력입니다.' };
+    return { validate: false, message: '정기결제시 customer_uid는 필수입력입니다.' };
   }
   
   return { validate: true, message: '' };
