35ac2ca62a8ce87ee807067ca1ec14ebb29dfcbb
deedee <mymusese@gmail.com>
2018-09-27 14:20:59 +0900

    [example/android] 안드로이드 production 환경에서 로컬 파일 경로 이슈
    - 안드로이드 production 환경에서 로컬 파일을 불러오지 못하는 이슈 발생
    - 안드로이드 production 환경에서 웹뷰 source를 따로 지정하는 코드 작성

--- src/components/Certification/index.js
+++ src/components/Certification/index.js
@@ -1,12 +1,10 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { WebView } from 'react-native';
+import { WebView, Platform } from 'react-native';
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
 
 import ErrorOnParams from '../ErrorOnParams';
-
-import { validateProps } from '../../utils';
 
 class Certification extends React.Component {
   static propTypes = {
@@ -90,7 +88,7 @@ class Certification extends React.Component {
 
   render() {
     const { userCode } = this.props;
-    const source = Platform.OS === 'android' ? {uri: "file:///android_asset/html/certification.html"} : require('../../html/certification.html');
+    const source = Platform.OS === 'android' && __DEV__ ? { uri: 'file:///android_asset/html/certification.html' } : require('../../html/certification.html'); // https://github.com/facebook/react-native/issues/505
 
     if (userCode) {
       return (
