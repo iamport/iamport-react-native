88907f7e4718899af5389ca91702536e0f133b05
deedee <mymusese@gmail.com>
2018-09-27 15:16:13 +0900

    [코드정리]

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 
-import { Platform } from 'react-native';
-import { setCustomSourceTransformer } from 'react-native/Libraries/Image/resolveAssetSource';
 import Payment from './components/Payment';
 import Certification from './components/Certification';
-
-// https://github.com/facebook/react-native/issues/505#issuecomment-273472688
-setCustomSourceTransformer(function (resolver) {
-
-  if (Platform.OS === 'android'
-    && !resolver.serverUrl
-    && !resolver.bundlePath
-    && resolver.asset.type === 'html') {
-    resolver.bundlePath = '/android_asset/';
-  }
-
-  return resolver.defaultAsset();
-});
 
 const IMP = {
   Payment,
