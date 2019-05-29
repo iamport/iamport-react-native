da2f4d830c2555d66d6a928ccf53844513811de3
deedee <mymusese@gmail.com>
2019-05-29 16:09:10 +0900

    가상계좌 발급 성공시 판단 URL 추가
    - KG이니시스, LG 유플러스, 나이스 등 사용시 가상계좌 발급 성공했을때 url은 https://service.iamport.kr/payments/vbank
    - 해당 url인지 여부 체크해 가상계좌 발급 성공 완료했는지 체크하는 로직 작성
    - imp_success 또는 success가 쿼리에 없지만, 어차피 imp_uid로 아임포트에 결제 성공 여부를 조회해야 정확한 것이기 때문에 무시

--- src/components/Payment/index.ios.js
+++ src/components/Payment/index.ios.js
@@ -175,6 +175,7 @@ class Payment extends React.Component {
   isUrlMatchingWithIamportUrl(url) {
     if (url.includes('https://service.iamport.kr/payments/fail')) return true;
     if (url.includes('https://service.iamport.kr/payments/success')) return true;
+    if (url.includes('https://service.iamport.kr/payments/vbank')) return true; // KG 이니시스, LG 유플러스, 나이스 가상계좌 발급성공
     return false;
   }
   
