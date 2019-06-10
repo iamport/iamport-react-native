8446c5556a9f63fd34cd312f447f89e870058e11
deedee <mymusese@gmail.com>
2019-06-10 15:24:40 +0900

    앱 소개 오타 수정(뷰 -> 리액트 네이티브)

--- example/src/Home/index.js
+++ example/src/Home/index.js
@@ -11,7 +11,7 @@ export default function Home({ navigation }) {
       <View style={container}>
         <View style={header.container}>
           <Text style={header.title}>아임포트 테스트</Text>
-          <Text style={header.text}>아임포트 뷰 네이티브 모듈 테스트 화면입니다.</Text>
+          <Text style={header.text}>아임포트 리액트 네이티브 모듈 테스트 화면입니다.</Text>
           <Text style={header.text}>아래 버튼을 눌러 결제 또는 본인인증 테스트를 진행해주세요.</Text>
         </View>
         <View style={button.container}>
