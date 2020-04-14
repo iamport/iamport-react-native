5a7084779f4cb0f56dde4d39b4c397c30d172205
deedee <mymusese@gmail.com>
2020-04-14 11:21:48 +0900

    웹뷰 로딩 시간을 javascript inject후까지로 연장

---
+++ src/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const viewStyles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  loadingContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    flex: 1,
+    zIndex: 1,
+  },
+  webViewContainer: {
+    flex: 1,
+    zIndex: 0,
+  },
+});
