826656bce023ac09f58b7110c1195ec1f24129c3
Yongwook Choi <leo@chai.finance>
2023-01-17 14:23:50 +0900

    Update iamport javascript SDK

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -77,8 +77,7 @@ namespace IMPConst {
     <meta http-equiv='content-type' content='text/html; charset=utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
 
-    <script type='text/javascript' src='https://code.jquery.com/jquery-latest.min.js' ></script>
-    <script type='text/javascript' src='https://cdn.iamport.kr/js/iamport.payment-1.2.0.js'></script>
+    <script type='text/javascript' src='https://cdn.iamport.kr/v1/iamport.js'></script>
   </head>
   <body></body>
 </html>
