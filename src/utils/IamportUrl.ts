72deb1be113121d99bd71446cb710d39b38f3a41
Hyukgyu Kwon <me@kwon.hk>
2023-01-20 16:06:27 +0900

    fix unable to open itms-appss problem

--- src/utils/IamportUrl.ts
+++ src/utils/IamportUrl.ts
@@ -13,8 +13,10 @@ class IamportUrl {
     this.scheme = url.split('://', 1)[0];
     let splittedUrl = [this.scheme, url.slice(this.scheme.length + 3)];
     if (Platform.OS === 'ios') {
-      this.path =
-        this.scheme === 'itmss' ? `https://${splittedUrl[1]}` : this.url;
+      // this.path = this.url;
+      this.path = this.scheme.startsWith('itms')
+        ? `https://${splittedUrl[1]}`
+        : this.url;
     } else if (Platform.OS === 'android') {
       if (this.isAppUrl()) {
         if (this.scheme.includes('intent')) {
