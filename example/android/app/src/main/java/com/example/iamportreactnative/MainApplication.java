74f4ab87cbfa84c6089e97ca891978f169d9b39f
Hyukgyu Kwon <me@kwon.hk>
2023-04-24 15:01:12 +0900

    fix: make android example work in upgraded react-native

--- example/android/app/src/main/java/com/example/iamportreactnative/MainApplication.java
+++ example/android/app/src/main/java/com/example/iamportreactnative/MainApplication.java
@@ -7,10 +7,10 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.iamportreactnative.BuildConfig;
 import com.iamportreactnative.IamportReactNativePackage;
 
 public class MainApplication extends Application implements ReactApplication {
